fa3e9eaf8fd4866124ae637f4342ff54ddd680dc
FrancineJones <francine.jones@hotmail.co.nz>
2016-05-09 19:38:14 +1200

    fixed interrupted scroll

--- javascript.js
+++ javascript.js
@@ -1,99 +1,72 @@
-$(window).on('wheel', _.throttle(animationFunction, 2600, {
-  'leading': true,
-  'trailing': false
-}))
+$(window).on('wheel', animationFunction);
 
+var timer = false;
 
-
-function animationFunction (e) {
-  
-  if(e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) { //alternative options for wheelData: wheelDeltaX & wheelDeltaY
-    //scroll down
-    var currentClassNumber = parseInt($('body').attr("class").split("")[10]);
-    
-    var currentClass = $('body').attr("class").split(" ")[0];
-    var newClass = "background" + (currentClassNumber + 1);
-    
-    // console.clear();
-    // console.log("Current class is " + currentClass)
-    // console.log("new class is " +newClass);
-    
-    $("body").removeClass(currentClass);
-    $("body").addClass(newClass);
-
-    if ($("body").hasClass("background6")) {
-        $("body").removeClass("background6").addClass("background5");
-    }
-
-  } else {
-    //scroll up
+function upAnimation(){
     var currentClassNumber = parseInt($('body').attr("class").split("")[10]);
     
     var currentClass = $('body').attr("class").split(" ")[0];
     var newClass = "background" + (currentClassNumber - 1);
-    
-    // console.clear();
-    // console.log("Current class is " + currentClass)
-    // console.log("new class is " +newClass);         
-    
+   
     $("body").removeClass(currentClass);
     $("body").addClass(newClass);
 
     if ($("body").hasClass("background0")) {
         $("body").removeClass("background0").addClass("background1");
     }
-
-    // $("body").bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function() {
-    
-    //     $("body").addClass("stop");
-
-    // })
-  }
+    setTimeout(function() {
+      timer = false;
+    }, 2600);
 }
 
-
-  // $(window).on("swipe",function(){
-  //   alert("swiped");
-  // });                       
-  
-  
-
-$(window).swipe( {
-  swipeUp:function(event, direction, distance, duration) {
+function downAnimation() {
     var currentClassNumber = parseInt($('body').attr("class").split("")[10]);
     
     var currentClass = $('body').attr("class").split(" ")[0];
     var newClass = "background" + (currentClassNumber + 1);
-    
-    // console.clear();
-    // console.log("Current class is " + currentClass)
-    // console.log("new class is " +newClass);
-    
+   
     $("body").removeClass(currentClass);
     $("body").addClass(newClass);
 
     if ($("body").hasClass("background6")) {
         $("body").removeClass("background6").addClass("background5");
+    }
+     setTimeout(function() {
+      timer = false;
+    }, 2600);
+}
+
+function animationFunction (e) {
+  
+  if(e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) { 
+    if(timer == false){
+      timer = true;
+      downAnimation();
+    }
+  } else {
+    if(timer == false){
+      timer = true;
+      upAnimation();
+    }
+    
+
+  }
+}
+             
+
+$(window).swipe( {
+  swipeUp:function(event, direction, distance, duration) {
+    if(timer == false){
+      timer = true;
+      downAnimation();  
     }
     
   },
   swipeDown:function(event, direction, distance, duration) {
-      var currentClassNumber = parseInt($('body').attr("class").split("")[10]);
-    
-    var currentClass = $('body').attr("class").split(" ")[0];
-    var newClass = "background" + (currentClassNumber - 1);
-    
-    // console.clear();
-    // console.log("Current class is " + currentClass)
-    // console.log("new class is " +newClass);         
-    
-    $("body").removeClass(currentClass);
-    $("body").addClass(newClass);
-
-    if ($("body").hasClass("background0")) {
-        $("body").removeClass("background0").addClass("background1");
+    if(timer == false){
+      timer = true;
+      upAnimation();
     }
-    
   },
   click:function(event, target) { 
   },
